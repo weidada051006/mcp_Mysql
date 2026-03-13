7ed3e0e0710422427d3575a44b7ebadaad94fb28
weidada051006 <weidada051006@gmail.com>
2026-03-13 10:56:01 +0800

    Delete do_push.bat

--- do_push.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d "d:\nlp2mysql"
-git init
-git add .
-git commit -m "Initial commit: nlp2mysql" 2>nul || git commit -m "Update nlp2mysql" --allow-empty
-git branch -M main
-git remote remove origin 2>nul
-git remote add origin https://github.com/weidada051006/mcp_Mysql.git
-git push -u origin main
